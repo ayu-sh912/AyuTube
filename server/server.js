eff9493dedb2170e36a325beec71a6abca95b194
Ayush Agrawal <ayushagrawal1234.ips@gmail.com>
2025-12-31 23:02:23 +0530

    Backend setup complete and environment files ignored

---
+++ server/server.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const cors = require("cors");
+
+dotenv.config();
+
+const app = express();
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+// Test Route
+app.get("/", (req, res) => {
+  res.send("AyuTube Backend is Running 🚀");
+});
+
+// MongoDB Connection
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    console.log("MongoDB Connected");
+    app.listen(process.env.PORT || 5000, () =>
+      console.log("Server running on port 5000")
+    );
+  })
+  .catch((err) => console.log(err));
